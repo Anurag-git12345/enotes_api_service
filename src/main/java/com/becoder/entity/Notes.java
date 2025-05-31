3919edd9ae0b30ff6f6c9c0e84947cf6517512ce
Anurag Katare <katareanurag1999@gmail.com>
2025-05-31 17:23:52 +0530

    copy notes with enotes

--- src/main/java/com/becoder/entity/Notes.java
+++ src/main/java/com/becoder/entity/Notes.java
@@ -14,10 +14,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Builder
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
