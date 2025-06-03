64477f370d747d244230e0c4709b8c112090c42b
Anurag Katare <katareanurag1999@gmail.com>
2025-06-03 15:54:30 +0530

    email-exist in enotes user data

--- src/main/java/com/becoder/exception/GlobalExceptionHandler.java
+++ src/main/java/com/becoder/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -52,5 +53,15 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<?> handleFileNotFoundException(FileNotFoundException e) {
 //		return new ResponseEntity<>(e.getErrors(), HttpStatus.BAD_REQUEST);
 		return CommonUtil.createErrorResponse(e.getMessage(), HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(ExistDataException.class)
+	public ResponseEntity<?> handleExistDataException(ExistDataException e) {
+		return CommonUtil.createErrorResponse(e.getMessage(), HttpStatus.CONFLICT);
+	}
+	
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<?> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+		return CommonUtil.createErrorResponse(e.getMessage(), HttpStatus.BAD_REQUEST);
 	}
 }
