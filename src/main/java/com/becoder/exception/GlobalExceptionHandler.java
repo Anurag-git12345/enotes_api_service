7aa214a98b3171b235d2a392935a9a90475bc039
Anurag Katare <katareanurag1999@gmail.com>
2025-06-03 15:22:25 +0530

    role based user registration module

--- src/main/java/com/becoder/exception/GlobalExceptionHandler.java
+++ src/main/java/com/becoder/exception/GlobalExceptionHandler.java
@@ -21,6 +21,12 @@ public class GlobalExceptionHandler {
 //		return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 		return CommonUtil.createErrorResponseMessage(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<?> handleIllegalArgumentException(IllegalArgumentException e) {
+		log.error("GlobalExceptionHandler :: handleException ::", e.getMessage());
+		return CommonUtil.createErrorResponseMessage(e.getMessage(), HttpStatus.BAD_REQUEST);
+	}
 
 	@ExceptionHandler(NullPointerException.class)
 	public ResponseEntity<?> handleNullPointerException(Exception e) {
