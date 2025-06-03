427deb0ae5c1119ee00e1cceef1a626242253014
Anurag Katare <katareanurag1999@gmail.com>
2025-06-03 17:47:06 +0530

    Enotes- Send Email Account Register Confirmation

--- src/main/java/com/becoder/service/impl/UserServiceImpl.java
+++ src/main/java/com/becoder/service/impl/UserServiceImpl.java
@@ -7,11 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
+import com.becoder.dto.EmailRequest;
 import com.becoder.dto.UserDto;
 import com.becoder.entity.Role;
 import com.becoder.entity.User;
 import com.becoder.repository.RoleRepository;
 import com.becoder.repository.UserRepository;
+import com.becoder.service.EmailService;
 import com.becoder.service.UserService;
 import com.becoder.util.Validation;
 
@@ -30,8 +32,11 @@ public class UserServiceImpl implements UserService{
 	@Autowired
 	private ModelMapper mapper;
 	
+	@Autowired
+	private EmailService emailService;
+	
 	@Override
-	public Boolean register(UserDto userDto) {
+	public Boolean register(UserDto userDto) throws Exception {
 		
 		validation.userValidation(userDto);
 		User user = mapper.map(userDto, User.class);
@@ -40,9 +45,29 @@ public class UserServiceImpl implements UserService{
 		
 		User saveUser = userRepo.save(user);
 		if (!ObjectUtils.isEmpty(saveUser)) {
+			// send email
+			emailSend(saveUser);
 			return true;
 		}
 		return false;
+	}
+
+	private void emailSend(User saveUser) throws Exception {
+		
+		String message = "Hi,<b>"+saveUser.getFirstName()+"</b> "
+				+ "<br> Your account register sucessfully.<br>"
+				+"<br> Click the below link verify & Active your account <br>"
+				+"<a href='#'>Click Here</a> <br><br>"
+				+"Thanks,<br>Enotes.com"
+				;
+		
+		EmailRequest emailRequest = EmailRequest.builder()
+						.to(saveUser.getEmail())
+						.title("Account Creating Confirmation")
+						.subject("Account Created Success")
+						.message(message)
+						.build();
+					emailService.sendEmail(emailRequest);	
 	}
 
 	private void setRole(UserDto userDto, User user) {
