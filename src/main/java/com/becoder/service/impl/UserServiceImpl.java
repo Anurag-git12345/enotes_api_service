54dcb15259ce6ec4d5e52f25b7ca662c907f31ab
Anurag Katare <katareanurag1999@gmail.com>
2025-06-06 14:04:43 +0530

    login with spring security

--- src/main/java/com/becoder/service/impl/UserServiceImpl.java
+++ src/main/java/com/becoder/service/impl/UserServiceImpl.java
@@ -5,10 +5,17 @@ import java.util.UUID;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
+import com.becoder.config.security.CustomUserDetails;
 import com.becoder.dto.EmailRequest;
+import com.becoder.dto.LoginRequest;
+import com.becoder.dto.LoginResponse;
 import com.becoder.dto.UserDto;
 import com.becoder.entity.AccountStatus;
 import com.becoder.entity.Role;
@@ -37,6 +44,12 @@ public class UserServiceImpl implements UserService{
 	@Autowired
 	private EmailService emailService;
 	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
 	@Override
 	public Boolean register(UserDto userDto,String url) throws Exception {
 		
@@ -50,6 +63,7 @@ public class UserServiceImpl implements UserService{
 				.verificationCode(UUID.randomUUID().toString())
 				.build();
 		user.setStatus(status); 
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
 		User saveUser = userRepo.save(user);
 		if (!ObjectUtils.isEmpty(saveUser)) {
 			// send email
@@ -84,6 +98,27 @@ private void emailSend(User saveUser, String url) throws Exception {
 		List<Integer> reqRoleId = userDto.getRoles().stream().map(r -> r.getId()).toList();
 		List<Role> roles = roleRepo.findAllById(reqRoleId);
 		user.setRoles(roles);
+	}
+
+	@Override
+	public LoginResponse login(LoginRequest loginRequest) {
+		
+		Authentication authenticate = authenticationManager.authenticate(
+						new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+		
+		if (authenticate.isAuthenticated()) {
+			CustomUserDetails customUserDetails = 
+					(CustomUserDetails)authenticate.getPrincipal();
+			
+			String token = "safdghhfdssaghnggsdsgfvswaefqwaef";
+			
+			LoginResponse loginResponse = LoginResponse.builder()
+							.user(mapper.map(customUserDetails.getUser(), UserDto.class))
+							.token(token)
+							.build();
+			return loginResponse;
+		}
+		return null;
 	}
 
 }
