6cfe086644c1b058cfcfb6abd86c16ed894ad004
Anurag Katare <katareanurag1999@gmail.com>
2025-06-09 16:08:43 +0530

    Password change user account

--- src/main/java/com/becoder/service/UserService.java
+++ src/main/java/com/becoder/service/UserService.java
@@ -1,14 +1,8 @@
 package com.becoder.service;
 
-import com.becoder.dto.LoginRequest;
-import com.becoder.dto.LoginResponse;
-import com.becoder.dto.UserDto;
+import com.becoder.dto.PasswordChangeRequest;
 
 public interface UserService {
 
-	public Boolean register(UserDto userDto, String url) throws Exception;
-
-	public LoginResponse login(LoginRequest loginRequest);
-
-	
+	public void ChangePassword(PasswordChangeRequest passwordRequest);
 }
