0fb16aa971db9dcfca3cc4a82215ba798d424f4d
Anurag Katare <katareanurag1999@gmail.com>
2025-06-10 16:08:49 +0530

    Search Notes

--- src/main/java/com/becoder/service/impl/NotesServiceImpl.java
+++ src/main/java/com/becoder/service/impl/NotesServiceImpl.java
@@ -205,6 +205,22 @@ public class NotesServiceImpl implements NotesService {
 	}
 
 	@Override
+	public NotesResponse getNotesByUserSearch(Integer pageNo, Integer pageSize,String keyword) {
+		// 10 = 5,5 = 2 pages
+		Integer userId = CommonUtil.getLoggedInUser().getId();
+		Pageable pageable = PageRequest.of(pageNo, pageSize);
+		Page<Notes> pageNotes = notesRepo.searchNotes(keyword,userId, pageable);
+
+		List<NotesDto> notesDto = pageNotes.get().map(n -> mapper.map(n, NotesDto.class)).toList();
+
+		NotesResponse notes = NotesResponse.builder().notes(notesDto).pageNo(pageNotes.getNumber())
+				.pageSize(pageNotes.getSize()).totalElements(pageNotes.getTotalElements())
+				.totalPages(pageNotes.getTotalPages()).isFirst(pageNotes.isFirst()).isLast(pageNotes.isLast()).build();
+
+		return notes;
+	}
+	
+	@Override
 	public void softDeleteNotes(Integer id) throws Exception {
 		
 		Notes notes = notesRepo.findById(id)
